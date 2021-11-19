25804043cdc9bd6e7c0b5fb7a9c0b12ce4dd7e58
Madrani Yahya <madrani.yahya@gmail.com>
2021-11-19 14:50:52 -0400

    code avant fichier .h

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
+/*
+  Titre      : Blink
+  Auteur     : Yahya Madrani
+  Date       : 17/11/2021
+  Description: Programme blink pour Arduino Maker 1000
+  Version    : 0.0.1
+*/
+
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+
+  pinMode(PIN_LED, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  digitalWrite(PIN_LED, HIGH);
+  delay(1000);
+
+  digitalWrite(PIN_LED, LOW);
+  delay(1000);
+
 }
