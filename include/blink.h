6dcb6bbb45c98054921b895909b5a3e734f12d91
Madrani Yahya <madrani.yahya@gmail.com>
2021-11-25 11:04:21 -0400

    Ajouté prepoc debug + rendre le code plus modulaire + ajouter la config PIO pour ESP8266

--- include/blink.h
+++ include/blink.h
@@ -1,7 +1,21 @@
-void blink(int temp){
-  digitalWrite(PIN_LED, HIGH);
-  delay(temp);
+/**
+ * @brief Simple fonction pour On/Off une LED
+ * 
+ * @param pin broche de sortie brancher a une LED
+ * @param temp le temps entre les on/off
+ */
+void blink(int pin, int temp){
+	// Block on
+	digitalWrite(pin, HIGH);
+	delay(temp);
+	#ifdef DEBUG
+		Serial.printf("LED sur le PIN %d est on\n", pin);
+	#endif
 
-  digitalWrite(PIN_LED, LOW);
-  delay(temp);
+	// Block off
+	digitalWrite(pin, LOW);
+	delay(temp);
+	#ifdef DEBUG
+		Serial.printf("LED sur le PIN %d est off\n", pin);
+	#endif
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,31 @@
   Titre      : Blink
   Auteur     : Yahya Madrani
   Date       : 17/11/2021
-  Description: Programme blink pour Arduino Maker 1000
+  Description: Programme blink pour le test des uC
   Version    : 0.0.1
 */
+
+#define DEBUG               // Si definie la console aura des message de debeugage
 
 #include <Arduino.h>
 #include "blink.h"
 
+#define LED_PIN LED_BUILTIN // Definition comme preprocesseur pour faciliter le changement du pin
+#define BLINK_TIME  250     // Le temps entre ON et OFF
+
 void setup() {
 
-  pinMode(PIN_LED, OUTPUT);
-  
+	Serial.begin(9600);       // Configuration frequence de sortie 
+	pinMode(LED_PIN, OUTPUT); // Configuration du PIN LED integré comme sortie
+	#ifdef DEBUG
+		Serial.printf("PIN %d est configuré comme sortie\n", LED_PIN);
+	#endif
+
 }
 
 void loop() {
 
-  blink(10000);
+	// Appel de notre fonctionblink dans notre fichier entete blink.h
+	blink(LED_PIN, BLINK_TIME);
 
 }
